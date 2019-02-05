39225a5f7388956b5fd8ed42f4d4dbc31766519b
Kvaido <nasimi@mamed-zade.com>
2019-02-05 22:50:18 +0300

    Update make_raid.sh

--- make_raid.sh
+++ make_raid.sh
@@ -5,5 +5,6 @@ parted -s /dev/md0 mklabel gpt
 parted /dev/md0 mkpart primary ext4 0% 50%
 parted /dev/md0 mkpart primary ext4 50% 100%
 for i in $(seq 1 2); do mkfs.ext4 /dev/md0p$i; done
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm.conf
 mkdir -p /raid/backup{1,2}
 for i in $(seq 1 2); do mount /dev/md0p$i /raid/backup$i; done
