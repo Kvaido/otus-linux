bb30090fc06c4009d09174412499ca1e1b764bd2
Kvaido <nasimi@mamed-zade.com>
2019-02-19 22:48:34 +0300

    Update status.sh

--- homework4/status.sh
+++ homework4/status.sh
@@ -11,9 +11,26 @@ while :
 		ps aux| grep -v grep | grep squid > /dev/null
 		#Передаем статус последней выподненной команды
 		RESULT=$?
+		curl -x localhost.localdomain:3128 http://google.com
+		CHECK=$?
 		#Начинаем сравнение
-		if [ "${RESULT}" -eq "0" ] ; then
+		#Если все хорошо, то продолжаем работу.
+		if [ "${RESULT}" -eq "0" ] && [ "${CHECK}" -eq "0" ] ; then
 			echo "`date`: Служба $SERVICE работает штатно." | awk '{print $0}' >> $WATCHDOG_LOG
+			sleep 2
+		#Если служба запущена, но  	
+		elif [ "${RESULT}" -eq "0" ] && [ "${CHECK}" -ne "0" ] ; then
+			echo "`date`: ВНИМАНИЕ!!! Служба $SERVICE запущена, однако не функционирует как надо! Пытаюсь перезапустить с прежними настрйоками!" | awk '{print $0}' >> $WATCHDOG_LOG
+			#Переписываем востанавливаем конфигурацию путем замещения измененного
+			cp /etc/squid/squid.conf.default /etc/squid/squid.conf
+			#Перезапускаем службу squid
+			systemctl restart $SERVICE
+			sleep 2
+			#Читаем последнюю строку в журнале событий и передаем переменной
+			ALARM=`tail -n 1 $WATCHDOG_LOG`
+			#Отправляем письмо
+			echo "$ALARM" | mail -s "SOMETHING IS WRONG" your@mail.ru
+			#Перезапускаем службу squid
 			sleep 2
 		else
 			echo "`date`: ВНИМАНИЕ!!! Служба $SERVICE не запущена! Запускаю!" | awk '{print $0}' >> $WATCHDOG_LOG
@@ -22,7 +39,7 @@ while :
 			#Отправляем письмо
 			echo "$ALARM" | mail -s "PROXY IS DOWN" your@mail.ru
 			#Перезапускаем службу squid
-			systemctl restart squid
+			systemctl restart $SERVICE
 			sleep 2
 		fi
 		sleep 20
