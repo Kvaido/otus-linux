fd1e9ecc11ac02777b47f6f935570cb58acf157a
Kvaido <nasimi@mamed-zade.com>
2019-06-21 23:21:30 +0300

    Update centralServer.sh

--- homework21/centralServer.sh
+++ homework21/centralServer.sh
@@ -8,7 +8,7 @@ systemctl start firewalld
 systemctl enable firewalld
 firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --reload
-echo -e '0.0.0.0 via 192.168.0.1\n192.168.1.0/26 via 192.168.0.1\n192.168.255.0/26 via 192.168.0.1\n192.168.254.0/26 via 192.168.0.1' > /etc/sysconfig/network-scripts/route-eth1
-echo "DEFROUTE=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0 
+#echo -e '192.168.1.0/26 via 192.168.0.1\n192.168.255.0/26 via 192.168.0.1\n192.168.254.0/26 via 192.168.0.1' > /etc/sysconfig/network-scripts/route-eth1
+echo "DEFROUTE=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0
 echo "GATEWAY=192.168.0.1" >> /etc/sysconfig/network-scripts/ifcfg-eth1
 systemctl restart network
